b0087f468644e950ffb72158ec546e7d65291c71
Octowl <alsaff1987@gmail.com>
2016-05-10 12:51:52 -0400

    Synchronize data models

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var server = http.createServer();
 var models = require('./models');
 var Promise = require('bluebird');
 
+
 server.on('request', require('./app'));
 
 Promise.all([
-  // models.User.sync({}),
-  // models.Page.sync({})
+  models.Place.sync(),
+  models.Hotel.sync(),
+  models.Restaurant.sync(),
+  models.Activity.sync()
 ])
 .then(function () {
   server.listen(3001, function () {
     console.log('Server is listening on port 3001!');
-  });
+});
 })
 .catch(console.error);
