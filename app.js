ac5a08a204bb097ec19c366940a1f7bcbfa0a189
Octowl <alsaff1987@gmail.com>
2016-05-10 14:41:45 -0400

    Use new route for index

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+var router = require('./routes');
+
 module.exports = app;
 
 app.set('views', path.join(__dirname, './views'));
@@ -17,9 +19,7 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res, next){
-    res.render('index', {});
-})
+app.use(router);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
